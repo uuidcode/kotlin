6cabd3a50148b392c019c9ee0892a8bf7eede819
uuidcode <uuidcode@gmail.com>
2016-03-28 15:08:22 +0900

    .

--- src/com/github/uuidcode/kotlin/data.kt
+++ src/com/github/uuidcode/kotlin/data.kt
@@ -4,14 +4,18 @@ data class User(val name: String = "", val age: Int = 0) {
     var phone: String = ""
     val userId: Long = 0
 
+    fun hello(): String {
+        return "HI"
+    }
+
 }
 
 fun main(args: Array<String>) {
     var user = User("java", 20)
     println(user.name)
-
     user.phone = "000-0000-0000"
 
     println(user.phone)
     println(user.userId)
+    println("AAAABBBB${user.hello()}DDDD")
 }
