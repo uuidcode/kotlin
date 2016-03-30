4db407f643a9e7833f91296c17f18d0db80b0ad2
uuidcode <uuidcode@gmail.com>
2016-03-30 17:50:34 +0900

    .

--- src/test/kotlin/com/github/uuidcode/PersonTest.kt
+++ src/test/kotlin/com/github/uuidcode/PersonTest.kt
@@ -1,12 +1,20 @@
 package com.github.uuidcode
 
 import com.github.uuidecode.Person
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class PersonTest {
     @Test
     fun test() {
-       var person = Person()
-        println(person.name)
+        var person = Person("A", Integer(10))
+        var (name, age) = person
+
+        assertThat(name, `is`("A"))
+        assertThat(age, `is`(Integer(10)))
+
+        assertThat(person.component1(), `is`("A"))
+        assertThat(person.component2(), `is`(Integer(10)))
     }
 }
